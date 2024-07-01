c7861ae11c096a7e24c4428c8e72f23b60e984a9
Jorah <169576348+0xjorah@users.noreply.github.com>
2024-07-01 20:57:05 +0800

    chore: add tailwindcss

--- packages/ui/src/app/layout.tsx
+++ packages/ui/src/app/layout.tsx
@@ -1,4 +1,7 @@
-export const metadata = {
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
   title: 'Spectre',
   description: 'Spectre liquidity protocol on Aleo: Stake credits, earn rewards, and borrow assets.',
 }
---
+++ packages/ui/tailwind.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from 'tailwindcss'
+
+const config = {
+  darkMode: ['class'],
+  content: ['./src/**/*.{ts,tsx}'],
+} satisfies Config
+
+export default config
