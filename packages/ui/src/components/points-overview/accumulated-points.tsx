468fa7616efaddc55c9bf48353cb7726d934ce4d
Jorah <169576348+0xjorah@users.noreply.github.com>
2024-08-09 17:02:12 +0800

    feat: update points overview

--- packages/ui/src/components/points-overview/accumulated-points.tsx
+++ packages/ui/src/components/points-overview/accumulated-points.tsx
@@ -2,7 +2,14 @@
 
 import * as dn from 'dnum'
 import { useTranslations } from 'next-intl'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '~/components/ui/tooltip'
 import { Skeleton } from '~/components/ui/skeleton'
+import { CircleHelpIcon } from 'lucide-react'
 
 export function AccumulatedPoints() {
   const t = useTranslations('PointsOverview.AccumulatedPoints')
@@ -10,7 +17,19 @@ export function AccumulatedPoints() {
 
   return (
     <div>
-      <div className="font-medium text-xs sm:text-sm">{t('title')}</div>
+      <div className="flex items-center font-medium text-xs sm:text-sm">
+        <span>{t('title')}</span>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <CircleHelpIcon className="w-3 h-3 lg:w-3.5 lg:h-3.5 ml-1" />
+            </TooltipTrigger>
+            <TooltipContent className="max-w-xs">
+              <p>{t('description')}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
       <div className="mt-1 font-semibold text-lg sm:text-xl">
         {isLoading ? (
           <Skeleton className="w-40">&nbsp;</Skeleton>
