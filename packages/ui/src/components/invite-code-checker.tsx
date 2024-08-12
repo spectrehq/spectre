9c24d9562900a17dbbe2a5430f45e7d182745dff
Jorah <169576348+0xjorah@users.noreply.github.com>
2024-08-12 14:45:02 +0800

    feat: update invite code checker

--- packages/ui/src/components/invite-code-checker.tsx
+++ packages/ui/src/components/invite-code-checker.tsx
@@ -3,6 +3,9 @@
 import { useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
 import { useLocalStorage } from 'usehooks-ts'
+import { useAccount } from '~/hooks/use-account'
+import { useInviterByCode } from '~/hooks/use-inviter-by-code'
+import { usePointsState } from '~/hooks/use-points-state'
 
 export function InviteCodeChecker() {
   const searchParams = useSearchParams()
@@ -14,7 +17,21 @@ export function InviteCodeChecker() {
     0
   )
 
+  const { address } = useAccount()
+  const { data: userPointsState } = usePointsState(address)
+
+  const { data: inviter } = useInviterByCode(Number(inviteCode))
+
   useEffect(() => {
+    if (userPointsState === undefined) return
+
+    const isNewUser = (userPointsState?.height ?? 0n) === 0n
+
+    if (!isNewUser) {
+      removeValue()
+      return
+    }
+
     if (value) {
       if (inviteCode) {
         if (value !== Number(inviteCode)) {
@@ -23,12 +40,24 @@ export function InviteCodeChecker() {
           window.location.search = url.search
         }
       }
-    } else if (inviteCode) {
-      setValue(Number(inviteCode))
+    } else if (inviteCode && inviter !== undefined) {
+      if (inviter && inviter !== address) {
+        setValue(Number(inviteCode))
+      } else {
+        const url = new URL(window.location.href)
+        url.searchParams.delete('invite_code')
+        window.location.search = url.search
+      }
     }
-  }, [inviteCode, setValue, value])
-
-  // TODO: remove invite code
+  }, [
+    address,
+    inviteCode,
+    inviter,
+    removeValue,
+    setValue,
+    userPointsState,
+    value,
+  ])
 
   return <></>
 }
