023f9afd0cf525d32047a59bda65d064c054f9a4
Jorah <169576348+0xjorah@users.noreply.github.com>
2024-08-23 18:46:01 +0800

    update

--- packages/ui/src/components/lock-widget/lock-form.tsx
+++ packages/ui/src/components/lock-widget/lock-form.tsx
@@ -98,7 +98,8 @@ export function LockForm() {
     if (isSuccess) {
       form.reset()
       void queryClient.refetchQueries({
-        predicate: ({ queryKey }) => queryKey.includes(address),
+        predicate: ({ queryKey }) =>
+          queryKey.includes(address) || queryKey.includes('blockHeight'),
       })
     }
   }, [address, form, isSuccess, queryClient])
